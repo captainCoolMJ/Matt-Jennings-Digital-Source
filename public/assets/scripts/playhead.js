e5ff1399ad3c24ee31a75f4a41c3fedbdf0ab0fc
Matt <mattj52990@gmail.com>
2019-07-26 13:49:50 +0400

    Git caching

---
+++ public/assets/scripts/playhead.js
@@ -0,0 +1,188 @@
+/**************
+* PLAYHEAD.JS *
+***************/
+
+
+// Some polyfills
+(function() {
+  if (!Event.prototype.preventDefault) {
+	Event.prototype.preventDefault=function() {
+	  this.returnValue=false;
+	};
+  }
+  if (!Event.prototype.stopPropagation) {
+	Event.prototype.stopPropagation=function() {
+	  this.cancelBubble=true;
+	};
+  }
+  if (!Element.prototype.addEventListener) {
+	var eventListeners=[];
+	
+	var addEventListener=function(type,listener /*, useCapture (will be ignored) */) {
+	  var self=this;
+	  var wrapper=function(e) {
+		e.target=e.srcElement;
+		e.currentTarget=self;
+		if (listener.handleEvent) {
+		  listener.handleEvent(e);
+		} else {
+		  listener.call(self,e);
+		}
+	  };
+	  if (type=="DOMContentLoaded") {
+		var wrapper2=function(e) {
+		  if (document.readyState=="complete") {
+			wrapper(e);
+		  }
+		};
+		document.attachEvent("onreadystatechange",wrapper2);
+		eventListeners.push({object:this,type:type,listener:listener,wrapper:wrapper2});
+		
+		if (document.readyState=="complete") {
+		  var e=new Event();
+		  e.srcElement=window;
+		  wrapper2(e);
+		}
+	  } else {
+		this.attachEvent("on"+type,wrapper);
+		eventListeners.push({object:this,type:type,listener:listener,wrapper:wrapper});
+	  }
+	};
+	var removeEventListener=function(type,listener /*, useCapture (will be ignored) */) {
+	  var counter=0;
+	  while (counter<eventListeners.length) {
+		var eventListener=eventListeners[counter];
+		if (eventListener.object==this && eventListener.type==type && eventListener.listener==listener) {
+		  if (type=="DOMContentLoaded") {
+			this.detachEvent("onreadystatechange",eventListener.wrapper);
+		  } else {
+			this.detachEvent("on"+type,eventListener.wrapper);
+		  }
+		  break;
+		}
+		++counter;
+	  }
+	};
+	Element.prototype.addEventListener=addEventListener;
+	Element.prototype.removeEventListener=removeEventListener;
+	if (HTMLDocument) {
+	  HTMLDocument.prototype.addEventListener=addEventListener;
+	  HTMLDocument.prototype.removeEventListener=removeEventListener;
+	}
+	if (Window) {
+	  Window.prototype.addEventListener=addEventListener;
+	  Window.prototype.removeEventListener=removeEventListener;
+	}
+  }
+})();
+
+/* CORE */
+var playHead = (function() {
+	'use strict';
+
+	var valX = 0,
+		valY = 0;
+
+	// Global listener to retrieve values
+	window.addEventListener('scroll', returnVals);
+	function returnVals() {
+		valX = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
+		valY = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+	}
+
+	var setTracks = (function(tracks, globalOptions) {
+		var globalSettings;
+
+		if( globalOptions !== 'undefined' ) {
+
+			globalSettings = globalOptions;
+
+		}
+
+		tracks.forEach(function(track) {
+
+			if( globalOptions !== 'undefined' ) {
+
+				for( var prop in globalOptions ) {
+					track[prop] = globalOptions[prop]; 
+				}
+
+			}
+
+			setTrack(track);
+
+		});
+
+	});
+
+	var setTrack = (function (options) {
+
+		// Setting default options from call
+		var inRange = Math.floor(parseInt(options.range.in)),
+			outRange = Math.floor(parseInt(options.range.out)),
+			playIn = options.playIn,
+			playOut = options.playOut,
+			orientation = options.orientation,
+			destroy = !!options.destroy,
+			scrollVal = 0;
+
+		// Trigger to make sure functions only fire once
+		var hasTriggered = false;
+
+		// Check to make sure the value is positive
+		if( outRange <= inRange ) {
+			console.error('Range must be a positive value!');
+		}
+
+		// Adding scroll listener
+		window.addEventListener('scroll', handleRanges, false);
+		handleRanges();
+
+		function handleRanges() {
+
+			// Determines which value to receive
+			scrollVal = orientation === 'landscape' ? valX : valY;
+
+			//console.log(scrollVal);
+
+			if( scrollVal >= inRange && scrollVal <= outRange ) {
+
+				if( !hasTriggered ) {
+				
+					playIn();
+
+				}
+
+				// Ensure playIn only calls once
+				hasTriggered = true;
+
+			} else if( scrollVal > outRange || scrollVal < inRange ) {
+
+				if( hasTriggered ) {
+
+					// Prevent another call
+					if( destroy ) {
+						window.removeEventListener('scroll', handleRanges);
+					}
+				
+					playOut();
+				
+				}
+
+				// Reset playIn
+				hasTriggered = false;
+
+			} else {
+				return false;
+			}
+
+		}
+
+	});
+
+	return {
+		setTrack: setTrack,
+		setTracks: setTracks
+	};
+
+})();
---
+++ public/assets/scripts/script.js
@@ -0,0 +1,423 @@
+(function(jQuery) {
+	'use strict';
+
+	var date = new Date();
+
+	var dateYear = date.getFullYear('yyyy');
+
+	var dateSpan = document.getElementById('date');
+
+	dateSpan.innerHTML = dateYear;
+
+	/* all methods */
+
+	// Add the navigation suggestion
+	var toggleNext = function() {
+		setTimeout(function() {
+			
+			//$('.nav-next').fadeToggle(2000);
+
+			$('.timeline-evt').addClass('active');
+
+		}, 1000);
+	};
+
+/*	// Sets each div to center of window
+	var setVerticalCenters = function(elements) {
+
+		var winHeight = $(window).outerHeight();
+
+		elements.each(function(i, el) {
+
+			var innerHeight = $(el).find('.inner_wrap').outerHeight();
+				
+			if( winHeight > innerHeight ) {
+				$(el).addClass('no_spill');
+			} else {
+				$(el).removeClass('no_spill');
+			}
+
+		});
+
+	};*/
+
+	// Adds a hover class so multiple elements can activate a hover
+	var toggleHoverClass = function($selector, $target, className) {
+
+		$selector.hover(function() {
+			$target.addClass(className);
+		},
+		function() {
+			$target.removeClass(className);
+		});
+
+	};
+
+	var loadTimeline = function loadTimeline(timeline) {
+
+		var formatDate = function formatDate(time) {
+			
+			var date = new Date(time),
+				monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+				month = monthNames[date.getMonth()],
+				year = date.getFullYear(),
+				ms = Date.parse(date);
+
+			// returns days since Jan 1, 1970
+			return {
+				'ms': ms,
+				'formatted' : month + ' ' + year
+			};
+		};
+
+		var Event = function(event) {
+			this.start = formatDate(event.from);
+			this.end = event.to.toLowerCase() !== 'present' ? formatDate(event.to) : formatDate(Date.now());
+			this.duration = this.end.ms - this.start.ms;
+			this.evt = event.action;
+			this.cat = event.category;
+			this.details = event.details;
+			this.$timelinePlot = $('<div class="timeline-evt">');
+		};
+
+		Event.prototype.build = function(index, startDate) {
+
+			if( this.end.ms < startDate ) {
+				return false;
+			}
+
+			var range = Date.now() - startDate,
+				width = Math.round(( this.duration / range ) * 100),
+				leftPos = Math.round(((this.start.ms - startDate) / range) * 100);
+
+			if( formatDate(Date.now()).ms === this.end.ms ) {
+				this.end.formatted = 'Present';
+			}
+
+			var $timelineDescription = $('<div class="time-evt-description">'),
+				$title = $('<h4 class="plot-title">').text(this.evt),
+				$range = $('<p class="plot-date">').text( this.start.formatted + ' - ' + this.end.formatted ),
+				$details = $('<p class="plot-details">').text(this.details);
+
+			width = width > 100 ? 100 : width;
+			leftPos = leftPos < 0 ? 0 : leftPos;
+
+			this.$timelinePlot.addClass('timeline-'+this.cat);
+
+			$timelineDescription
+				.append($title)
+				.append($range)
+				.append($details);
+
+			/*this.$timelinePlot.css({
+				'left': leftPos + '%',
+				'width': width + '%',
+				'top': 40 * index + 'px'
+			});*/
+
+			this.addEventListeners();
+
+			this.$timelinePlot.append($timelineDescription);
+
+			return this.$timelinePlot;
+
+		};
+
+		Event.prototype.addEventListeners = function() {
+
+			this.$timelinePlot.hover(function() {
+				$(this).toggleClass('hover');
+			});
+
+		};
+
+		var init = function init() {
+
+			var sortedEvents = [],
+				sortedEndEvents = [],
+				sortedStartEvents = [];
+
+			timeline.forEach(function(timeEvent) {
+
+				var evt = new Event(timeEvent);
+
+				sortedEvents.push(evt);
+
+			});
+
+			sortedEvents.sort(function(a, b) {
+				return b.start.ms - a.start.ms;
+			});
+
+			var oneYear = 31556925974,
+				yearRange = Date.now() - (oneYear * 8);
+
+			var work = sortedEvents.filter(function(event) {
+					return event.cat === 'work';
+				}),
+				life = sortedEvents.filter(function(event) {
+					return event.cat === 'life';
+				});
+
+			var $workTimeline = $('#timeline-work'),
+				$lifeTimeline = $('#timeline-life');
+
+			work.forEach(function(evt, i) {
+				$workTimeline.append(evt.build(i, yearRange));
+			});
+
+			life.forEach(function(evt, i) {
+				$lifeTimeline.append(evt.build(i, yearRange));
+			});
+
+		};
+
+		return {
+			init: init()
+		};
+
+	};
+
+	/*
+	* Smooth scrolling main navigation
+	***********************************/
+	$('a[href*=#]:not([href=#])').click(function(e) {
+		if( location.pathname.replace(/^\//,'' ) == this.pathname.replace(/^\//,'') && location.hostname == this.hostname ) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+			if( target.length ) {
+				$('html,body').stop().animate({
+					scrollTop: target.offset().top
+				}, 350, 'easeInOutQuart');
+				e.preventDefault();
+			}
+
+		}
+	});
+
+	/* Form validation */
+	function checkValid(fieldVal) {
+
+		if( fieldVal.value.length > 0 ) {
+
+			if( fieldVal.fieldName === 'email' ) {
+				
+				var email = fieldVal.value,
+					invalidChars = " /:,;",
+					badChar,
+					atPos,
+					periodPos;
+
+				// Check for invalid characters
+				for (var i=0; i<invalidChars.length; i++) {
+					
+					badChar = invalidChars.charAt(i);
+
+					if (email.indexOf(badChar,0) > -1) {
+						return false;
+					}
+
+				}
+				
+				// Check for first @ symbol
+				atPos = email.indexOf("@",1);
+				
+				if (atPos == -1) {
+					return false;
+				}	
+
+				// Check for only one "@" symbol
+				if (email.indexOf("@",atPos+1) != -1) {
+					return false;
+				}
+
+				// Make sure there's a period after the @
+				periodPos = email.indexOf(".",atPos);
+				if (periodPos == -1) {
+					return false;
+				}
+
+				// Ensure at least two characters after "."
+				if (periodPos+3 > email.length)	{
+					return false;
+				}
+
+				// If all the above pass correctly
+				fieldVal.valid = true;
+
+
+			} else {
+
+				fieldVal.valid = true;
+
+			}
+
+		} else {
+
+			fieldVal.valid = false;
+
+		}
+
+	}
+
+	// Form processing
+	function processForm() {
+
+		var formFieldArr = [],
+			$errorList = $('<ul>'),
+			formHeight = $('.form-contact').css('height'),
+			$loaderDiv = $('<div class="loaderDiv">').css({
+				'height': formHeight
+			});
+
+		$('.form-contact').css('height', formHeight).append($loaderDiv);
+
+		// For each required field, create object and add to the field array
+		$('.form-contact .form-required').each(function(i, field){
+
+			var fieldObj = {
+				'fieldName': $(field).attr('name'),
+				'text': $(field).attr('id').replace('form-', ''),
+				'value': $(field).val(),
+				'valid': false
+			};
+
+			formFieldArr.push(fieldObj);
+
+		});
+
+		// Validate all required fields
+		formFieldArr.map(checkValid);
+
+		// Filter out the correct fields
+		var invalidFields = formFieldArr.filter(function(field){
+			return field.valid === false;
+		});
+
+		// If there are any errors
+		if( invalidFields.length ) {
+
+			$('.form-contact').css('height', 'auto');
+			$loaderDiv.remove();
+
+			invalidFields.forEach(function(field){
+
+				var $errorLi = $('<li>'),
+					errVal = field.text.toLowerCase(),
+					errStr = 'Please enter valid ' + errVal;
+
+				$errorLi.text(errStr);
+				$errorList.append($errorLi);
+
+			});
+
+			$('.form-error').html($errorList);
+
+		} else {
+
+			// All fields are valid, prepare data string for form request
+			var dataPrepArr = formFieldArr.map(function(field){
+				var str = encodeURIComponent(field.fieldName)+'='+encodeURIComponent(field.value);
+				return str;
+			});
+
+			var preppedDataStr = dataPrepArr.join('&');
+
+			// Disable editing form
+			$('.form-contact input, .form-contact textarea').attr('disabled', true);
+
+			$('.form-error').text('');
+
+			$.ajax({
+				type : "POST",
+				data : preppedDataStr,
+				dataType : 'json',
+				url: "scripts/sendmail.php",
+				success: function(data) {
+					
+					$('.form-contact').html('<p>' + data.msg + '</p>');
+					$('.form-text').remove();
+
+				},
+				error: function(err) {
+
+					$('.form-contact').html('<p>There has been an error. Please <a href="mailto:info@redmeded.com" title="Send us an email">send us an email</a> instead.</p><p>' + err.status + ': ' + err.statusText + '</p>');
+					$('.form-text').remove();
+
+				},
+				complete: function() {
+
+					$loaderDiv.remove();
+					$('.form-contact input, .form-contact textarea').attr('disabled', false);
+
+				}
+			});
+
+		}
+
+	}
+
+	/*$(window).load(function() {
+			
+		setTimeout(function() {
+
+			setVerticalCenters($('.sct'));
+
+		}, 200);
+
+	});*/
+
+	$(document).ready(function() {
+
+		// Load up the site data
+		$.getJSON('data/site.json', function(data){
+			//loadWork(data.work);
+			loadTimeline(data.timeline);
+
+		});
+
+		// Targets, selector to apply class to, class to apply
+		toggleHoverClass($('.title, .logo'), $('.logo'), 'hover');
+		toggleHoverClass($('header, .main_nav'), $('header'), 'hover');
+
+		toggleNext();
+
+		$('#toggleMenu').on('click', function() {
+			
+			$('body').addClass('menu-active');
+
+			$('body').one('click', function() {
+				$('body').removeClass('menu-active');
+			});
+
+			return false;
+
+		});
+
+		var bounds = {
+			'left': Math.round($('.timelines').offset().left),
+			'top': Math.round($('.timelines').offset().top),
+			'right': Math.round($('.timelines').offset().left + $('.timelines').outerWidth()),
+			'bottom': Math.round($('.timelines').offset().top + $('.timelines').outerHeight())
+		};
+
+		// contact form
+		$('.form-contact .form-submit').click(function(e){
+
+			e.preventDefault();
+
+			processForm();
+
+		});
+
+		// should only hover within the bounds of the timeline square
+
+		
+
+		//$('.timelines').on('mousemove', timelineMouseMove);
+
+	});
+
+}($));

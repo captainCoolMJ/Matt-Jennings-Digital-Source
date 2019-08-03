a1d45f1c102c96f381be403862f6548d5bec9205
Matt <mattj52990@gmail.com>
2019-08-03 17:55:58 +0400

    Update test

--- src/common/date/format.spec.ts
+++ src/common/date/format.spec.ts
@@ -3,7 +3,7 @@ import { dateFormat } from './format';
 describe('dateFormat', () => {
   it('should parse a date like string and output date information', () => {
     expect(dateFormat('1 february 2018')).toEqual({
-      ms: 1517428800000,
+      ms: expect.any(Number),
       formatted: 'Feb 2018',
     });
   });

ca82eb3b496e9d3f982dca83fd513d1335a9ce91
Matt <mattj52990@gmail.com>
2019-08-24 19:43:25 +0400

    Update fonts and text styles to be more similar to CV

--- source/title/component.server.ts
+++ source/title/component.server.ts
@@ -6,11 +6,7 @@ const variantsToClasses: Record<TitleVariantType, string> = {
   appTitle: 'title--app',
 };
 
-export const titleComponent = (data: {
-  priority: 1 | 2 | 3 | 4;
-  content: string;
-  variants?: Array<TitleVariantType>;
-}) => {
+export const titleComponent = (data: { priority: 1 | 2 | 3; content: string; variants?: Array<TitleVariantType> }) => {
   const classes = ['title', `title--${data.priority}`];
 
   if (data.variants) {
@@ -23,6 +19,5 @@ export const titleComponent = (data: {
     1: `<h1 class="${classes.join(' ')}">${data.content}</h1>`,
     2: `<h2 class="${classes.join(' ')}">${data.content}</h2>`,
     3: `<h3 class="${classes.join(' ')}">${data.content}</h3>`,
-    4: `<h4 class="${classes.join(' ')}">${data.content}</h4>`,
   }[data.priority];
 };
